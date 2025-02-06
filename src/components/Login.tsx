7901e802bcf9ec3847d53c638de0fe865580dab9
Magalie Chetrit <1594036+BluePraise@users.noreply.github.com>
2025-02-06 10:29:52 -0500

    Login form connected to API with errorhandling

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,20 +1,40 @@
 import React, { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 
-const Login: React.FC = () => {
+interface LoginProps {
+  onClose?: () => void; // close the modal if success
+}
+
+
+const Login: React.FC<LoginProps> = ({onClose}) => {
   const { login } = useAuth();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     login(name, email);
+    setLoading(true);
+    setError(null);
+
+    try {
+      await login(name, email);
+      // close the modal on successful login:
+      if (onClose) onClose();
+    } catch (err) {
+      setError(`Failed to login: ${err.message}`);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <div className="text-center shadow-sm flex flex-col items-center w-2xl my-8 p-8">
-        <h1 className="text-2xl my-8">Login to start</h1>
         <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+            <h2 className="text-2xl my-8">Login to start</h2>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
             <input
                 className="mb-4 border border-gray-400 p-2"
                 type="text"
